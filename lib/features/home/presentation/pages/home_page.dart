564c4f200fdab443195ba65e3ade139cac800d57
Murilo Formiga <murilo.formiga@popcode.com.br>
2020-07-05 18:03:24 -0300

    refact: pasta do coordintor global

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hiso/core/components/coordinator/coordinator_provider.dart';
+import 'package:hiso/core/coordinator/coordinator_provider.dart';
 import 'package:hiso/features/auth/coordinator/auth_coordinator.dart';
 import 'package:hiso/features/home/presentation/bloc/home_bloc.dart';
 
