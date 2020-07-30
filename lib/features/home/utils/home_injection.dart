c1a0188d6e086be95a8ea53a7cdcd5b80ea4b5f6
Lucas José Mendes <llucasmendes@hotmail.com>
2020-07-30 14:55:34 -0300

    fix: home injection

--- lib/features/home/utils/home_injection.dart
+++ lib/features/home/utils/home_injection.dart
@@ -1,3 +1,4 @@
+import 'package:hiso/features/home/coordinator/home_coordinator_impl.dart';
 import 'package:hiso/features/home/data/datasources/pacient_datasource.dart';
 import 'package:hiso/features/home/data/datasources/validation_datasource.dart';
 import 'package:hiso/features/home/data/repositories/pacient_repository_impl.dart';
@@ -36,6 +37,12 @@ void initHome() {
       networkInfo: dep(),
     ),
   );
+
+  // Coordinator
+  dep.registerFactory(
+    () => HomeCoordinatorImpl(),
+  );
+
   dep.registerLazySingleton<PacientRepository>(
     () => PacientRepositoryImpl(
       pacientDataSource: dep(),
