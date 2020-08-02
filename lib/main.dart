66d6f322d8f0ce2d201c3526e379a5f3c14196d7
Murilo Formiga <murilo.formiga@popcode.com.br>
2020-08-01 21:10:53 -0300

    fix: tema readicionado ao app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hiso/core/navigator/routes.dart';
+import 'package:hiso/core/theme/app_theme.dart';
 import 'package:hiso/features/auth/presentation/bloc/splash/splash_cubit.dart';
 import 'package:hiso/features/auth/presentation/pages/splash_page.dart';
 import 'package:hiso/injection_container.dart' as di;
@@ -23,6 +24,7 @@ class HisoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hiso',
+      theme: AppTheme.hisoTheme,
       home: BlocProvider<SplashCubit>(
         create: (_) => dep<SplashCubit>()..verifyCurrentUser(),
         child: SplashPage(),
