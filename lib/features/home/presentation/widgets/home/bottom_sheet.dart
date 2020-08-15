57974cd9faeee07538ce71d9738bc61faa7380d5
llucasmendes <llucasmendes@hotmail.com>
2020-08-15 17:51:02 -0300

    feat: bottomsheet

---
+++ lib/features/home/presentation/widgets/home/bottom_sheet.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BottomSheet {
+  BottomSheet({
+    this.context,
+    this.child,
+  });
+  final dynamic context;
+  final Widget child;
+  void showBottonSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            color: Color(0xFF737373),
+            height: 180,
+            child: Container(
+              child: child,
+              decoration: BoxDecoration(
+                color: Theme.of(context).canvasColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(10),
+                  topRight: const Radius.circular(10),
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
